fc7cfc03b3820424243bcc2243e4b99559c81f75
jackYuan <jackyuan110@gmail.com>
2022-11-13 15:30:37 -0800

    111-11-13
    variation
    testing with forming up geojson in one piece

--- js/core.js
+++ js/core.js
@@ -1,24 +1,4 @@
 
-function caseColour(number){
-	if(number>=200){
-		return '#99000d';		
-	}
-	else if(number>=100 && number<200){
-		return '#ef3b2c';
-	}
-	else if(number>=50 && number<100){
-		return '#fc9272';
-	}
-	else if(number>=25 && number<50){
-		return '#fcbba1';
-	}
-	else if(number>0 && number<25){
-		return '#fee0d2';
-	}	
-	else{
-		return '#fff5f0';
-	}			
-}
 
 
 
@@ -33,51 +13,38 @@ $('document').ready(function(){
 
 		success:function(data){
 			result1=data;
-			// console.log(result1[0]);
-			// $('#result').html(result1);	
+			var jsonlist="[";
 			for(let i=0;i<result1.length;i++){
 				//0 is JSON, 1 is postal code, 2 is number of cases
 				// $('#test').append(result1[i][0]);
 				var fill=caseColour(result1[i][2]);
-				// console.log(result1[i][1]);
 				console.log(fill);
 				var myStyle = {
 					"fillColor":fill,
 					"fillOpacity":0.75,
-					// "fillOpacity":result1[i][1],
 					"color": 'black',
 					"weight": 1.5,
 					"dashArray":3
-				    // "opacity": 0
-				    // "opacity": result1[i][1]
-
 				};
 				//base.php json encoded the entire array but not individual objects within the array
 				//must JSON.parse the specific array key that refers to the json object again
 				//or error "invaild geoJSON object" will occur
-				var geoj=JSON.parse(result1[i][0]);
-				// console.log(geoj);
-				// console.log(JSON.parse(result1[0][0]));
-				// console.log(result1[i][1]);
-				var postal_code=L.geoJSON(geoj, {
-					style: myStyle
-				}).addTo(map);
-
-				$('path').attr('id',result1[i][1]);
-
-				// $('path').ready(function(){
-				// 	$(this).attr('id',result1[i][1]);
-				// });				
-				// console.log(postal_code[geoj.properties]);
-				// console.log(geoj.properties.postal-fsa);
-				
+				// var geoj=JSON.parse(result1[i][0]);
+				var geoj=result1[i][0];
+				// jsonlist.push(geoj);
+				jsonlist+=geoj;
+				jsonlist+=',';
 			}
-			// for(let i=0;i<result1.length;i++){
-			// 	$('path').each(function(index){
-			// 		$(this).attr('id',result1[i][1]);
-			// 	})
-			// }
-
+			//cut the last comma
+			jsonlist=jsonlist.slice(0,-1);
+			jsonlist+="]";
+			jsonlist=JSON.parse(jsonlist)
+			console.log(jsonlist);
+			// console.log(JSON.parse(jsonlist));
+			var postal_code=L.geoJSON(jsonlist, {
+				style:getStyle(2)
+			}).addTo(map);
+			
 			connSuccess=true;
 			console.log(connSuccess);
 			if(connSuccess){
@@ -114,6 +81,36 @@ $('document').ready(function(){
 });
 
 
+function getStyle(feature){
+	return{
+	"fillColor":caseColour(feature),
+	"fillOpacity":0.75,
+	"color": 'black',
+	"weight": 1.5,
+	"dashArray":3	
+	};
+}
+
+function caseColour(number){
+	if(number>=200){
+		return '#99000d';		
+	}
+	else if(number>=100 && number<200){
+		return '#ef3b2c';
+	}
+	else if(number>=50 && number<100){
+		return '#fc9272';
+	}
+	else if(number>=25 && number<50){
+		return '#fcbba1';
+	}
+	else if(number>0 && number<25){
+		return '#fee0d2';
+	}	
+	else{
+		return '#fff5f0';
+	}			
+}
 
 function highlightFeature(e) {
 	var layer = e.target;
