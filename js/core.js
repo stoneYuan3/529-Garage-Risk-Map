7c9b84f0fb87e80587a8b9103e6d5ac5dfd6bb78
jackYuan <jackyuan110@gmail.com>
2022-11-13 16:18:42 -0800

    111-11-13
    issue "add postal code as id to each postal code shape in html using jquery" solved

--- js/core.js
+++ js/core.js
@@ -37,20 +37,14 @@ $('document').ready(function(){
 			// $('#result').html(result1);	
 			for(let i=0;i<result1.length;i++){
 				//0 is JSON, 1 is postal code, 2 is number of cases
-				// $('#test').append(result1[i][0]);
 				var fill=caseColour(result1[i][2]);
-				// console.log(result1[i][1]);
 				console.log(fill);
 				var myStyle = {
 					"fillColor":fill,
 					"fillOpacity":0.75,
-					// "fillOpacity":result1[i][1],
 					"color": 'black',
 					"weight": 1.5,
 					"dashArray":3
-				    // "opacity": 0
-				    // "opacity": result1[i][1]
-
 				};
 				//base.php json encoded the entire array but not individual objects within the array
 				//must JSON.parse the specific array key that refers to the json object again
@@ -59,25 +53,19 @@ $('document').ready(function(){
 				// console.log(geoj);
 				// console.log(JSON.parse(result1[0][0]));
 				// console.log(result1[i][1]);
+
 				var postal_code=L.geoJSON(geoj, {
 					style: myStyle
 				}).addTo(map);
+				postal_code.bindPopup(result1[i][1]);
 
-				$('path').attr('id',result1[i][1]);
-
-				// $('path').ready(function(){
-				// 	$(this).attr('id',result1[i][1]);
-				// });				
-				// console.log(postal_code[geoj.properties]);
-				// console.log(geoj.properties.postal-fsa);
-				
+				//$('path').eq(i) is equvalent to $('path')[i]
+				//although [i] works fine with console log, it cannot have functions attached to it (ex, $('path')[i].attr(...)) leads to error "attr() is not a function"
+				$('g path').eq(i).attr('id',result1[i][1]);		
+					// console.log($('g path').length);
+					// console.log($('g path')[i]);
+					// console.log($('path[id]').length>0);
 			}
-			// for(let i=0;i<result1.length;i++){
-			// 	$('path').each(function(index){
-			// 		$(this).attr('id',result1[i][1]);
-			// 	})
-			// }
-
 			connSuccess=true;
 			console.log(connSuccess);
 			if(connSuccess){
@@ -85,6 +73,16 @@ $('document').ready(function(){
 				$('path').hover(
 					function(){
 						$(this).css("stroke-width","5");
+						console.log($(this).attr("id"));
+							// $.ajax({
+							// 	type:'POST',
+							// 	url:'server/base.php?request=1',
+							// 	data:{},
+							// 	dataType:'json',
+							// 	success:function(data){
+							// 		console.log("yee");
+							// 	}
+							// });									
 					},
 					function(){
 						$(this).css("stroke-width","1.5");
@@ -96,37 +94,23 @@ $('document').ready(function(){
 			console.log('You moron, you messed something up!');
 		}		
 	});
-	
-	// if(connSuccess){
-	// 	// map.on('click', highlightFeature);
-	// 	$('path').hover(
-	// 		function(){
-	// 			$(this).css("stroke-width","3");
-	// 		},
-	// 		function(){
-	// 			$(this).css("stroke-width","1.5");
-	// 		}			
-	// 	);
-	// }
-
-
 
 });
 
 
 
-function highlightFeature(e) {
-	var layer = e.target;
+// function highlightFeature(e) {
+// 	var layer = e.target;
 
-	layer.setStyle({
-		weight: 5,
-		color: '#666',
-		dashArray: '',
-		fillOpacity: 0.7
-	});
+// 	layer.setStyle({
+// 		weight: 5,
+// 		color: '#666',
+// 		dashArray: '',
+// 		fillOpacity: 0.7
+// 	});
 
-	layer.bringToFront();
-}
-function resetHighlight(e) {
-	geojson.resetStyle(e.target);
-}
+// 	layer.bringToFront();
+// }
+// function resetHighlight(e) {
+// 	geojson.resetStyle(e.target);
+// }
